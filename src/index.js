d749a29f2f01bd06451257b64daafecfdfde544f
decaffeinate <danny@goodeggs.com>
2016-12-21 11:56:38 -0800

    decaffeinate: Run post-processing cleanups on index.coffee and 2 other files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 import tv4 from 'tv4';
 
 const allowNull = (data, schema) => data === null && (schema.type === 'null' || schema.type.includes('null'));
