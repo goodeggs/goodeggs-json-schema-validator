e7f0ed3ae2d12abae9bc3c7f79b38ae0440b87b6
Camilla Teodoro <camillateodoro@hotmail.com>
2023-01-29 19:49:07 -0300

    Add type to data and schema

--- src/index.ts
+++ src/index.ts
@@ -25,29 +25,29 @@ const clonedTv4: GoodeggsJsonSchemaValidator = tv4.freshApi();
 const allowNull = (data: unknown, schema: JsonSchema): boolean =>
   data === null && (schema.type === 'null' || schema.type?.includes('null') === true);
 
-clonedTv4.addFormat('objectid', (data, schema) => {
+clonedTv4.addFormat('objectid', (data: unknown, schema: JsonSchema) => {
   if (allowNull(data, schema)) return null;
   if (typeof data === 'string' && /^[a-f\d]{24}$/i.test(data)) return null;
   return 'objectid expected';
 });
-clonedTv4.addFormat('date-time', (data, schema) => {
+clonedTv4.addFormat('date-time', (data: unknown, schema: JsonSchema) => {
   if (allowNull(data, schema)) return null;
   // Source: http://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
   const dateTimeRegex = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
   if (typeof data === 'string' && dateTimeRegex.test(data)) return null;
   return 'date-time, ISOString format, expected';
 });
-clonedTv4.addFormat('date', (data, schema) => {
+clonedTv4.addFormat('date', (data: unknown, schema: JsonSchema) => {
   if (allowNull(data, schema)) return null;
   if (typeof data === 'string' && /\d{4}-[01]\d-[0-3]\d/.test(data)) return null;
   return 'date, YYYY-MM-DD format, expected';
 });
-clonedTv4.addFormat('time', (data, schema) => {
+clonedTv4.addFormat('time', (data: unknown, schema: JsonSchema) => {
   if (allowNull(data, schema)) return null;
   if (typeof data === 'string' && /^[0-2][0-9]:[0-5][0-9](:[0-5][0-9])?$/.test(data)) return null;
   return 'time, HH:mm:ss or HH:mm format, expected';
 });
-clonedTv4.addFormat('email', (data, schema) => {
+clonedTv4.addFormat('email', (data: unknown, schema: JsonSchema) => {
   if (allowNull(data, schema)) return null;
   // Source: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
   const emailRegex =
@@ -55,7 +55,7 @@ clonedTv4.addFormat('email', (data, schema) => {
   if (typeof data === 'string' && emailRegex.test(data)) return null;
   return 'email expected';
 });
-clonedTv4.addFormat('non-negative-integer', (data, schema) => {
+clonedTv4.addFormat('non-negative-integer', (data: unknown, schema: JsonSchema) => {
   if (allowNull(data, schema)) return null;
   if (typeof data === 'string' && /^[0-9]+$/.test(data)) return null;
   return 'non negative integer expected';
